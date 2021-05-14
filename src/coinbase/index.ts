4068b23679ca0c69597d9ee9dbe6719fbe95eb95
Charles Rollin <charles.rollin@illuin.tech>
2021-05-14 18:58:23 +0200

    feat: persist fetched prices

--- src/coinbase/index.ts
+++ src/coinbase/index.ts
@@ -1,5 +1,14 @@
 import axios from 'axios';
+import * as path from 'path';
+import * as fse from 'fs-extra';
 import { DateTime } from 'luxon';
+
+// { BTC: { "1970-01-01": 1000 } }
+type PriceCache = Record<string, Record<string, number>>;
+
+const priceCache: PriceCache = fse.readJSONSync(
+  path.resolve(__dirname, '../../cache/spotPricesCache.json')
+);
 
 interface SpotPriceResult {
   data: {
@@ -9,9 +18,24 @@ interface SpotPriceResult {
   };
 }
 
+const updateCache = (asset: string, date: DateTime, price: number) => {
+  if (priceCache[asset] === undefined) {
+    priceCache[asset] = { [date.toISO()]: price };
+  } else {
+    priceCache[asset][date.toISO()] = price;
+  }
+  fse.writeJSONSync(path.resolve(__dirname, '../../cache/spotPricesCache.json'), priceCache);
+};
+
 export async function getSpotPriceHistory(asset: string, date: DateTime) {
+  const cachedPrice = priceCache[asset]?.[date.toISO()];
+  if (cachedPrice) {
+    return cachedPrice;
+  }
   const result = await axios.get<SpotPriceResult>(
     `https://api.coinbase.com/v2/prices/${asset}-EUR/spot?date=${date.toISO()}`
   );
-  return parseFloat(result.data.data.amount);
+  const price = parseFloat(result.data.data.amount);
+  updateCache(asset, date, price);
+  return price;
 }
